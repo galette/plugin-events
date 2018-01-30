99aac73b2635e808c16fdda6a1e15c53ea13f4ca
Johan Cwiklinski <jcwiklinski@teclib.com>
2018-01-30 14:04:31 +0100

    Add SQL scripts

---
+++ scripts/mysql.sql
@@ -0,0 +1,50 @@
+SET FOREIGN_KEY_CHECKS=0;
+
+--
+-- Table structure for table `galette_events_events`
+--
+
+DROP TABLE IF EXISTS galette_events_events;
+CREATE TABLE galette_events_events (
+  id_event int(10) NOT NULL,
+  name varchar(150) NOT NULL,
+  address varchar(150) NOT NULL default '',
+  zip varchar(10) NOT NULL default '',
+  town varchar(50) NOT NULL default '',
+  country varchar(50) default NULL,
+  begin_date date NOT NULL default '1901-01-01',
+  end_date date NOT NULL default '1901-01-01',
+  creation_date date NOT NULL default '1901-01-01',
+  has_meal tinyint(1) NOT NULL default 0,
+  is_meal_required tinyint(1) NOT NULL default 0,
+  has_lodging tinyint(1) NOT NULL default 0,
+  is_lodging_required tinyint(1) NOT NULL default 0,
+  is_open tinyint(1) NOT NULL default 1,
+  id_group int(10) default NULL,
+  PRIMARY KEY (id_event),
+  FOREIGN KEY (id_group) REFERENCES galette_groups (id_group) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Table structure for table `galette_events_bookings`
+--
+
+DROP TABLE IF EXISTS galette_events_bookings;
+CREATE TABLE galette_events_bookings (
+  id_event int(10) NOT NULL auto_increment,
+  id_adh int(10) unsigned NOT NULL,
+  booking_date date NOT NULL default '1901-01-01',
+  is_paid tinyint(1) NOT NULL default 0,
+  payment_amount  decimal(15, 2) default '0',
+  payment_method tinyint(3) unsigned NOT NULL default '0',
+  bank_name varchar(100) default NULL,
+  check_number varchar(50) default NULL,
+  has_meal tinyint(1) NOT NULL default 0,
+  number_people int(4) default NULL,
+  creation_date date NOT NULL default '1901-01-01',
+  PRIMARY KEY (id_event,id_adh),
+  FOREIGN KEY (id_event) REFERENCES galette_events_events (id_event) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (id_adh) REFERENCES galette_adherents (id_adh) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+SET FOREIGN_KEY_CHECKS=1;
---
+++ scripts/pgsql.sql
@@ -0,0 +1,60 @@
+--
+-- Table structure for table `galette_events_events`
+--
+
+DROP SEQUENCE IF EXISTS galette_events_events_id_seq;
+CREATE SEQUENCE galette_events_events_id_seq
+    START 1
+    INCREMENT 1
+    MAXVALUE 2147483647
+    MINVALUE 1
+    CACHE 1;
+
+DROP TABLE IF EXISTS galette_events_events CASCADE;
+CREATE TABLE galette_events_events (
+  id_event integer DEFAULT nextval('galette_events_events_id_seq'::text) NOT NULL,
+  name character varying(150) NOT NULL,
+  address character varying(150) NOT NULL default '',
+  zip character varying(10) NOT NULL default '',
+  town character varying(50) NOT NULL default '',
+  country character varying(50) default NULL,
+  begin_date date default '19010101' NOT NULL,
+  end_date date default '19010101' NOT NULL,
+  creation_date date default '19010101' NOT NULL,
+  has_meal boolean default FALSE,
+  is_meal_required boolean default FALSE,
+  has_lodging boolean default FALSE,
+  is_lodging_required boolean default FALSE,
+  is_open boolean default TRUE,
+  id_group integer REFERENCES galette_groups(id_group) ON DELETE RESTRICT ON UPDATE CASCADE default NULL,
+  PRIMARY KEY (id_event)
+);
+
+--
+-- Table structure for table `galette_events_bookings`
+--
+
+DROP SEQUENCE IF EXISTS galette_events_bookings_id_seq;
+CREATE SEQUENCE galette_events_bookings_id_seq
+    START 1
+    INCREMENT 1
+    MAXVALUE 2147483647
+    MINVALUE 1
+    CACHE 1;
+
+DROP TABLE IF EXISTS galette_events_bookings CASCADE;
+CREATE TABLE galette_events_bookings (
+  id_booking integer DEFAULT nextval('galette_events_bookings_id_seq'::text) NOT NULL,
+  id_event integer REFERENCES galette_events_events (id_event) ON DELETE CASCADE ON UPDATE CASCADE,
+  id_adh integer REFERENCES galette_adherents (id_adh) ON DELETE CASCADE ON UPDATE CASCADE,
+  booking_date date default '19010101' NOT NULL,
+  is_paid boolean default FALSE,
+  payment_amount real default '0',
+  payment_method smallint default '0' NOT NULL,
+  bank_name character varying(100) default NULL,
+  check_number character varying(50) default NULL,
+  has_meal boolean default FALSE,
+  number_people smallint default NULL,
+  creation_date date default '19010101' NOT NULL,
+  PRIMARY KEY (id_event,id_adh)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
