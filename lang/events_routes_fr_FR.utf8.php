57dd66aac803819e0081968c8134a30d096ac3c3
Johan Cwiklinski <jcwiklinski@teclib.com>
2018-01-30 15:15:54 +0100

    Add langs

---
+++ lang/events_routes_fr_FR.utf8.php
@@ -0,0 +1,11 @@
+<?php
+// This file was automatically generated on Tue Jan 30 14:14:42 2018 (GMT)
+// Don't modify it by hand, rather use the target lang from the Makefile.
+
+
+// ../_routes.php:53 
+$lang['events_routes']['/bookings'] = '/reservations';
+
+// ../_routes.php:40 
+$lang['events_routes']['/events'] = '/evenements';
+
