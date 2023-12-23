8bb791f2280c209195babb91e858bce4171aca02
Johan Cwiklinski <johan@x-tnd.be>
2023-12-23 08:29:36 +0100

    Update website URL

--- _routes.php
+++ _routes.php
@@ -9,7 +9,7 @@
  *
  * Copyright © 2018-2023 The Galette Team
  *
- * This file is part of Galette (http://galette.tuxfamily.org).
+ * This file is part of Galette (https://galette.eu).
  *
  * Galette is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -31,7 +31,7 @@
  * @copyright 2018-2023 The Galette Team
  * @license   http://www.gnu.org/licenses/gpl-3.0.html GPL License 3.0 or (at your option) any later version
  * @version   SVN: $Id$
- * @link      http://galette.tuxfamily.org
+ * @link      https://galette.eu
  */
 
 use Galette\Repository\Groups;
