8facf50cce24e2c5f57c9ed2cc27e35fb6c05ece
Johan Cwiklinski <johan@x-tnd.be>
2026-07-20 09:29:34 +0200

    Drop useless lib

--- webpack.config.js
+++ webpack.config.js
@@ -17,10 +17,8 @@ module.exports = {
   },
   externals: {
     // shows how we can rely on browser globals instead of bundling these dependencies,
-    // in case we want to access jQuery from a CDN or if we want an easy way to
-    // avoid loading all moment locales: https://github.com/moment/moment/issues/1435
-    jquery: 'jQuery',
-    moment: 'moment'
+    // in case we want to access jQuery from a CDN
+    jquery: 'jQuery'
   },
   devtool: 'source-map',
   module: {
